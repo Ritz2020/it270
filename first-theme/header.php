6818ef17f33d50da876362a79d11cad02350f55a
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-07-08 18:29:17 -0700

    Add files via upload

---
+++ first-theme/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ;?> >
+<head>
+<meta charset="<?php bloginfo('charset');?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title><?php bloginfo('name');?></title>
+<link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet" type="text/css">
+<?php wp_head();  ?> 
+<!-- this function will alow your plugin information, i.e. stylesheets, javastript, etc to display BEFORE the closing head  -->
+</head>
+<body <?php body_class(); ?>>
+<header>
+<div id="top">
+</div> 
+ <!--end top  -->
+<div id="inner-header">
+
+</div>
+<!-- end inner-header -->
+
+<div id="hero">
+
+</div>
+<!-- end hero -->
+</header>
