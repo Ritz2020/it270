4ff970d1b1f80499dc8f20e464b0fd5ff8842a16
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-07-14 11:26:16 -0700

    Add files via upload

---
+++ first-theme/page-tours.php
@@ -0,0 +1,32 @@
+<?php 
+get_header(); 
+// Template Name :Tours
+?>
+
+<div id="wrapper">
+<main>
+<!-- the index.php is assigned to the blog page!!! -->
+<!-- our question of the day is If we have any posts or pages, show them!!!-->
+<?php if (have_posts() ) : ?>
+<!--we need to show the posts by using a while loop in the world of PHP!!!  -->
+<?php while(have_posts()) : the_post();?>
+<?php the_content() ; ?>
+
+
+<?php endwhile ; ?>
+
+<?php else : ?>
+<h2>
+<?php echo  wpautop('Sorry, no posts were found!'); ?>
+</h2>
+<?php endif; ?>
+</main>
+
+<aside>
+</aside>
+</div>
+<!-- close wrapper -->
+
+<?php
+get_footer();
+?>
